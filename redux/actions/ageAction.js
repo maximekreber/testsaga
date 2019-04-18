592082b2c734436cd62e3cc42cd729574da6dd90
Kreber Maxime <krebermaxime@MacBook-Air-de-Matthieu.local>
2019-04-18 10:08:16 +0200

    CombineReducer Fix

--- redux/actions/ageAction.js
+++ redux/actions/ageAction.js
@@ -1,13 +1,12 @@
+export const mapStateToProps = state => {
+  return {
+    age: state.ageReducer.age
+  };
+};
 
- export const mapStateToProps = state => {
-    return {
-      age: state.age
-    };
+export const mapDispachToProps = dispatch => {
+  return {
+    onAgeUp: () => dispatch({ type: "AGE_UP", value: 1 }),
+    onAgeDown: () => dispatch({ type: "AGE_DOWN", value: 1 })
   };
-  
- export const mapDispachToProps = dispatch => {
-    return {
-      onAgeUp: () => dispatch({ type: "AGE_UP", value: 1 }),
-      onAgeDown: () => dispatch({ type: "AGE_DOWN", value: 1 })
-    };
-  };
+};
